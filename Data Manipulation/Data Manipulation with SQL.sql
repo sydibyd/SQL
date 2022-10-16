4942b41dcddce5d2c652c65eb891fdd35f5abb30
Saeid SHARIFY <35872299+sydibyd@users.noreply.github.com>
2022-10-16 18:43:04 +0330

    Update Data Manipulation with SQL.sql

--- Data Manipulation/Data Manipulation with SQL.sql
+++ Data Manipulation/Data Manipulation with SQL.sql
@@ -15,3 +15,22 @@ FROM `project_name.dataset_name.table_name`
 
 GROUP BY year
 ORDER BY year
+
+
+/* This query counts the total number of observations (trips) grouped by months and years */
+
+SELECT
+
+ # Extract years and months from a date_time (data type) comlum
+ EXTRACT(year FROM A_point_moment) AS trip_year,
+ EXTRACT(month FROM A_point_moment) AS trip_month,
+ 
+ # Count the number of observations (here, the number of A_point_moment or trips): 
+ COUNT(*) AS total_trips
+
+FROM `project_name.dataset_name.table_name`
+
+ # Group the output results by the extracted years and months:
+
+GROUP BY trip_year,trip_month
+ORDER BY trip_year,trip_month
