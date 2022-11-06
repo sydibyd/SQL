a877fb61f763b0219be9d46d629a63e15d7f972d
Saeid SHARIFY <35872299+sydibyd@users.noreply.github.com>
2022-11-06 16:24:34 +0330

    Update Geospatial observation over time.sql

--- Data Manipulation/Geospatial observation over time.sql
+++ Data Manipulation/Geospatial observation over time.sql
@@ -6,7 +6,7 @@ Date: 2022/10/16
 
 # ---- ---- ---- ---- ---- ---- ---- ----
 # Select a time interval in data and extract years, days and geo points of trips
-WITH trip_source AS
+WITH all_trips AS
 
 (
 
@@ -17,8 +17,7 @@ WITH trip_source AS
   ST_GEOGPOINT(start_station_latitude, start_station_longitude) AS geo_A_point,
   ST_GEOGPOINT(end_station_latitude, end_station_longitude) AS geo_B_point
 
-  FROM`bigquery-public-data.new_york_citibike.citibike_trips`
-  #FROM `project_name.dataset_name.table_name`
+  FROM `project_name.dataset_name.table_name`
 
   WHERE
   starttime
@@ -26,3 +25,22 @@ WITH trip_source AS
   AND '2020-12-31T23:59:59'
   
 ),
+
+
+# Set a register to calculate cycled miles over time (daily for each year)
+  distance_cycled AS
+
+(
+
+  SELECT
+  year,
+  day,
+
+  # Converting meters to miles:
+  ST_DISTANCE(geo_A_point, geo_B_point) * .000621 AS miles_cycled 
+  
+  FROM all_trips
+  
+),
+
+
