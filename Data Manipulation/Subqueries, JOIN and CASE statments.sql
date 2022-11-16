e6b352ea2423b6ec227d609360800508e9394553
Saeid SHARIFY <35872299+sydibyd@users.noreply.github.com>
2022-11-16 20:32:32 +0100

    Update Subqueries, JOIN and CASE statments.sql

--- Data Manipulation/Subqueries, JOIN and CASE statments.sql
+++ Data Manipulation/Subqueries, JOIN and CASE statments.sql
@@ -27,6 +27,8 @@ Two datasets are manipulated in this example:
 # SELECT
 # Select 3 columns and add them into the output table:
 # 1. warehouse_id 2. warehouse_name 3. num_of_orders (for each warehouse)
+# Add state and warehouse_alias strings together create warehouse_name
+# Count number of orders
 
 SELECT
   Warehouse.warehouse_id,
